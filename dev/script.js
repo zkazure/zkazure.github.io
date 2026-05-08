64015afc55e512152f80c3d36ed827d25e201c5c
Kazure Zheng <kazure@debian>
2026-05-09 01:01:23 +0800

    remove dev/

--- dev/script.js
+++
@@ -1,7 +0,0 @@
-function showPic(whichpic) {
-    let source = whichpic.getAttribute("href");
-    let placeholder = document.getElementById("placeholder");
-    placeholder.setAttribute("src", source);
-    console.log("good");
-    return ;
-}
